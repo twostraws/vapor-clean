1580eb4f6d4c025ab7b039c2ec1d600b0e0af13a
Paul Hudson <paul.hudson@gmail.com>
2018-02-27 13:59:21 +0000

    Adding template files.

---
+++ Package.swift
@@ -0,0 +1,15 @@
+// swift-tools-version:4.0
+import PackageDescription
+
+let package = Package(
+    name: "VaporApp",
+    dependencies: [
+        // 💧 A server-side Swift web framework.
+        .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0-rc"),
+    ],
+    targets: [
+        .target(name: "App", dependencies: ["Vapor"]),
+        .target(name: "Run", dependencies: ["App"]),
+        .testTarget(name: "AppTests", dependencies: ["App"]),
+    ]
+)
---
+++ Sources/App/routes.swift
@@ -0,0 +1,11 @@
+import Routing
+import Vapor
+
+/// Register your application's routes here.
+///
+/// [Learn More →](https://docs.vapor.codes/3.0/getting-started/structure/#routesswift)
+public func routes(_ router: Router) throws {
+    router.get("hello") { req in
+        return "Hello, world!"
+    }
+}

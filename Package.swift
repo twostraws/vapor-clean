1696e2392df2c472e5fe26d32bd2296e43a24de1
Paul Hudson <paul.hudson@gmail.com>
2019-05-20 21:20:00 +0100

    Updated for Vapor 3.3.

--- Package.swift
+++ Package.swift
@@ -1,11 +1,11 @@
-// swift-tools-version:4.0
+// swift-tools-version:5.0
 import PackageDescription
 
 let package = Package(
     name: "VaporApp",
     dependencies: [
         // 💧 A server-side Swift web framework.
-        .package(url: "https://github.com/vapor/vapor.git", .upToNextMinor(from: "3.1.0")),
+        .package(url: "https://github.com/vapor/vapor.git", .upToNextMinor(from: "3.3.0")),
     ],
     targets: [
         .target(name: "App", dependencies: ["Vapor"]),
